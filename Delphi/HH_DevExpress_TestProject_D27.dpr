b292758f8a56b7405f1143dbacd156247b35e301
santiagoIT <sburbano@itworks.ec>
2021-01-28 17:15:28 -0500

    Initial commit. Test project for Delphi Rio (10.3) and Delphi Sydney (10.4). Debug 32/64 bit.

---
+++ Delphi/HH_DevExpress_TestProject_D27.dpr
@@ -0,0 +1,14 @@
+program HH_DevExpress_TestProject_D27;
+
+uses
+  Vcl.Forms,
+  FrmMain in '..\FrmMain.pas' {Form4};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm4, Form4);
+  Application.Run;
+end.
---
+++ FrmMain.pas
@@ -0,0 +1,25 @@
+unit FrmMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, cxGraphics, cxControls, cxLookAndFeels, cxLookAndFeelPainters, cxContainer, cxEdit, dxSkinsCore, dxSkinOffice2019Colorful, cxLabel, cxClasses, cxLocalization;
+
+type
+  TForm4 = class(TForm)
+    cxLabel1: TcxLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+end.
